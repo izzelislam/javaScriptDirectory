daf351ab00e0ae60d459147d1353c0b6bdc6ff85
izzelislam <bangfkr002@gmail.com>
2019-12-04 06:38:20 +0700

    java script leason

---
+++ comparison.js
@@ -0,0 +1,13 @@
+//alert ('nama');
+
+var pmdg1=5;
+var pmdg2="5";
+var taghasil=document.getElementById('hasil');
+
+if (pmdg1 != pmdg2){
+	taghasil.innerHTML="true";
+}
+
+else {
+	taghasil.innerHTML="false";
+}
